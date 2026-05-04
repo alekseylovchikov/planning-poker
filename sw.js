0cd08119edd0ed74fe516541c0cf8713ad3f4664
Aleksei Lovchikov <a.lovchikov@apicworld.com>
2026-05-04 10:49:34 +0300

    fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10961377
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+  domain: '5gvci.com',
+  zoneId: 10961442,
+};
+self.lary = '';
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
